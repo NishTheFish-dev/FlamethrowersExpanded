21fc56d09bfca057a11c8b4159f0adf62fc0e1b8
NishTheFish <57572411+NishTheFish-dev@users.noreply.github.com>
2025-12-01 21:44:52 -0700

    Updated concentrated gel to be translucent

--- Content/Items/ConcentratedGel.cs
+++ Content/Items/ConcentratedGel.cs
@@ -1,3 +1,5 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -20,12 +22,30 @@ namespace FlamethrowersExpanded.Content.Items
             Item.ammo = AmmoID.Gel;
         }
 
+        public override bool PreDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, ref float rotation, ref float scale, int whoAmI)
+        {
+            Main.GetItemDrawFrame(Item.type, out Texture2D itemTexture, out Rectangle itemFrame);
+            Vector2 drawOrigin = itemFrame.Size() / 2f;
+            Vector2 drawPosition = Item.Bottom - Main.screenPosition - new Vector2(0, drawOrigin.Y);
+            Color drawColor = new Color(0, 30, 160) * 0.7f;
+            spriteBatch.Draw(itemTexture, drawPosition, itemFrame, drawColor, rotation, drawOrigin, scale, SpriteEffects.None, 0f);
+            return false;
+        }
+
+        public override bool PreDrawInInventory(SpriteBatch spriteBatch, Vector2 position, Rectangle frame, Color drawColor, Color itemColor, Vector2 origin, float scale)
+        {
+            Texture2D itemTexture = ModContent.Request<Texture2D>(Texture).Value;
+            Color color = new Color(0, 30, 160) * 0.7f;
+            spriteBatch.Draw(itemTexture, position, frame, color, 0f, origin, scale, SpriteEffects.None, 0f);
+            return false;
+        }
+
         public override void AddRecipes()
-		{
-			Recipe recipe = CreateRecipe();
-			recipe.AddIngredient(ItemID.Gel, 3);
-			recipe.AddTile(TileID.WorkBenches);
-			recipe.Register();
-		}
+        {
+            Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ItemID.Gel, 3);
+            recipe.AddTile(TileID.WorkBenches);
+            recipe.Register();
+        }
     }
 }
